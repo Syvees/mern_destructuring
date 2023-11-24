eda73cd0976abeecfc9f761349353b59ec1289cb
syvees_sazon <syvees_sazon@Syveess-Mac.local>
2023-11-24 10:04:02 -0800

    first commit

---
+++ core_predict_destructuring.js
@@ -0,0 +1,32 @@
+// PREDICT DESTRUCTURING
+
+/* PROBLEM 1
+Tesla
+Mercedes
+*/
+
+
+/* PROBLEM 2
+It will throw an error since name will have otherName as new variable.
+Elon
+*/
+
+
+/* PROBLEM 3
+12345
+It will throw an error because password is not included in the object person.
+*/
+
+
+/* PROBLEM 4
+5
+2
+*/
+
+
+/* PROBLEM 5
+value
+[1, 5, 1, 8, 3, 3]
+It will throw an error because lastTest object is not included.
+It will throw an error because lastTest object is not included.
+*/
